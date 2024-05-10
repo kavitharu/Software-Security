1908a3d054daac0bbeba232f2e1c3d16981515d0
kavi <38864058+kavitharu@users.noreply.github.com>
2024-05-10 19:05:04 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "pma";
+$password = "pma";
+$db = "onlineshop";
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+?>
